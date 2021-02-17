e85d7aef622d755787ba97c207bb6e6b0fc42164
Andres Barrios <barriosan95@gmail.com>
2021-02-17 01:34:26 -0500

    added initial side nav functionality with toggle

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ModalModule } from 'ngx-bootstrap/modal';
 import { AppRoutingModule } from './app-routing.module';
 
+//Serivces
+import { AdminSideNavService } from './services/admin-side-nav.service';
+
 // Components
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { AdminTopNavbarComponent } from './pages/admin/admin-top-navbar/admin.top.navbar.component';
+import { AdminSideNavTogglerComponent } from './pages/admin/admin-side-nav-toggler/admin.side.nav.toggler.component';
+import { AdminSideNavbarComponent } from './pages/admin/admin-side-nav/admin.side.nav.component';
 import { NavbarComponent } from './pages/home/navbar/navbar.component';
 import { IntroComponent } from './pages/home/intro/intro.component';
 import { ProjectsComponent } from './pages/home/projects/projects.component';
@@ -18,6 +24,9 @@ import { DashboardComponent } from './pages/admin/dashboard/dashboard.component'
   declarations: [
     AppComponent,
     HomeComponent,
+    AdminTopNavbarComponent,
+    AdminSideNavTogglerComponent,
+    AdminSideNavbarComponent,
     NavbarComponent,
     IntroComponent,
     ProjectsComponent,
@@ -30,7 +39,7 @@ import { DashboardComponent } from './pages/admin/dashboard/dashboard.component'
     ModalModule.forRoot(),
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AdminSideNavService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
